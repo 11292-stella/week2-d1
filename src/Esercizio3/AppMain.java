a47c390bdc9250b85dc76c42343caf5bedf75f60
11292-stella <stella.marucelli@gmail.com>
2025-05-12 18:01:47 +0200

    modifiche

--- src/Esercizio3/AppMain.java
+++ src/Esercizio3/AppMain.java
@@ -8,30 +8,38 @@ public class AppMain {
 
         Scanner scanner = new Scanner(System.in);
         ContoCorrente conto = new ContoCorrente(100.00,0);
-        ContoOnline  co = new ContoOnline(200.0,0,50);
+        ContoOnline  co = new ContoOnline(100.0,0,50);
 
-        try{
-            System.out.println("inserisci importo da prelevare");
-            double importo = scanner.nextDouble();
+        while (true) {
+            try {
+                System.out.println("inserisci importo da prelevare");
+                double importo = scanner.nextDouble();
 
-            conto.preleva(importo);
-            System.out.println("prelievo effettuato con successo");
+                conto.preleva(importo);
+                System.out.println("prelievo effettuato con successo");
 
-        } catch (BancaException e){
-            System.out.println("errore: " + e.getMessage());
+            } catch (BancaException e) {
+                System.out.println("errore: " + e.getMessage());
+                break;
+            }
         }
 
-        try{
-            System.out.println("Inserisci l'importo da prelevare dal ContoOnline:");
-            double importo2 = scanner.nextDouble();
-            co.preleva(importo2);
-            System.out.println("Prelievo da ContoOnline riuscito! Saldo: " + co.getSaldo());
-        }catch (BancaException e){
-            System.out.println("Errore ContoOnline: " + e.getMessage());
-        }
+        while (true) {
 
-        System.out.println("saldo attuale: " + conto.getSaldo());
-        System.out.println("numero di prelievi: " + conto.getContatorePrelievi());
+            try {
+                System.out.println("Inserisci l'importo da prelevare dal ContoOnline:");
+                double importo2 = scanner.nextDouble();
+                co.preleva(importo2);
+                System.out.println("Prelievo da ContoOnline riuscito! Saldo: " + co.getSaldo());
+            } catch (BancaException e) {
+                System.out.println("Errore ContoOnline: " + e.getMessage());
+            }
+
+
+            System.out.println("saldo attuale: " + conto.getSaldo());
+            System.out.println("numero di prelievi: " + conto.getContatorePrelievi());
+
+        }
 
 
 
